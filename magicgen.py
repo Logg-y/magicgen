9f5918f570a1b84d7a6291cb52d3be6283ca7a5c
logg-y <loggyc4@gmail.com>
2021-01-31 04:00:19 +0000

    fix astral harpoon in construction

--- magicgen.py
+++ magicgen.py
@@ -14,7 +14,7 @@ spellstokeep = [150, 167, 166, 165, 168, 169, 189, 190]
 
 # All spells below this ID get moved to unresearchable
 START_ID = 1300
-ver = "2.1.1"
+ver = "2.1.2"
 
 ALL_PATH_FLAGS = [utils.PathFlags(2 ** x) for x in range(0, 8)]
 
